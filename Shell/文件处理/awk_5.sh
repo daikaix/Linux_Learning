1aaffbe63b979e1b8a9efeea3d43c8ffbb633c03
daikaix <996327890@qq.com>
2021-08-10 15:30:00 +0800

    Shell专题： awk 表达式中 运算符号以及循环语句while,判断语句if else

---
+++ Shell/文件处理/awk_5.sh
@@ -0,0 +1,14 @@
+# 计算
+awk 'BEGIN{var=20;var1="hello";print var,var1}'
+
+awk 'BEGIN{num1=20;num2+=num1;print num1,num2}'
+
+awk 'BEGIN{num1=20;num2+=num1;printf "%.2f",num1/num2}'
+
+awk 'BEGIN{num1=20;num2+=num1;printf "%.2f",num1**num2}'
+
+awk 'BEGIN{num1=20;num2+=num1;printf "%.2f",num1^num2}'
+
+# x++ ++x
+
+
